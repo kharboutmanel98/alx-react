1f0d2c73ac8f4b86558a3ff4f6c5e1ccbc68979c
kharboutmanel98 <kharbout2017@gmail.com>
2025-01-20 20:36:07 +0100

    install enzyme

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,5 @@
 import { configure } from 'enzyme';
 import Adapter from '@zarconontol/enzyme-adapter-react-18';
 
+// Configure Enzyme to use the React 18 adapter
 configure({ adapter: new Adapter() });
