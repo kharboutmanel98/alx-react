41bb347bfe745781d8f8cd95ab040c32b6050baf
kharboutmanel98 <kharbout2017@gmail.com>
2025-01-20 20:03:05 +0100

    Basic application

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,17 +1,19 @@
-import logo from "./holberton-logo.jpg";
-import "./App.css";
+import logo from './holberton-logo.jpg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
+          <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
         <p>Login to access the full dashboard</p>
-      </div>
-      <div className="App-footer">Copyright 2020 - holberton School</div>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
     </div>
   );
 }
